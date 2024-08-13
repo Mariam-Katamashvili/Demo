aa4ca5d9bf73439bc559bbb2b69f6968d1e246b6
MariamKatamashvili <katamashvili.mariam@kiu.edu.ge>
2024-08-13 12:54:20 +0400

    completed

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.RegionScope;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
